d643139880286bf70883dc9e67e5253fe70b24f6
Kenyodarz <minajorge@hotmail.com>
2022-02-24 20:30:31 -0500

    Services Implemented

--- src/app/core/services/venta.service.ts
+++ src/app/core/services/venta.service.ts
@@ -1,9 +1,24 @@
+// Angular
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+// Servicio Generico
+import { CommonService } from './common.service';
+// Variable de Entorno
+import { API_URL } from 'src/environments/environment';
+// Modelo
+import { Venta } from 'src/app/core/models/venta';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class VentaService {
+export class VentaService extends CommonService<Venta, number> {
+  protected override URL_API: string = `${API_URL}/ventas`;
+  constructor(protected override http: HttpClient) {
+    super(http);
+  }
 
-  constructor() { }
+  obtenerSerie(): Observable<any> {
+    return this.http.get<any>(`${this.URL_API}/serie`);
+  }
 }
