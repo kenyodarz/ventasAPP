53ad4ec3d3d13b9f145177c16021a6e42109fd3d
Kenyodarz <minajorge@hotmail.com>
2022-02-24 21:12:57 -0500

    Components Created and routes updated

--- src/app/pages/resume/resume.module.ts
+++ src/app/pages/resume/resume.module.ts
@@ -6,10 +6,14 @@ import { PrimengModule } from 'src/app/shared/primeng.module';
 import { ResumeRoutingModule } from './resume-routing.module';
 import { ResumeComponent } from './resume.component';
 import { HomeComponent } from './components/home/home.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
+import { EmpleadoComponent } from './components/empleado/empleado.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { RegistrarVentaComponent } from './components/registrar-venta/registrar-venta.component';
 
 
 @NgModule({
-  declarations: [ResumeComponent, HomeComponent],
+  declarations: [ResumeComponent, HomeComponent, ClientesComponent, EmpleadoComponent, ProductosComponent, RegistrarVentaComponent],
   imports: [
     CommonModule,
     ResumeRoutingModule,
